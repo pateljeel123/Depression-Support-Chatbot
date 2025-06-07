c9c0a71bd13092c4ea193c6d42e2b3b156f46ead
takshpatel28 <takshpatel416@gmail.com>
2025-06-07 12:05:17 +0530

    updates

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -307,7 +307,7 @@ const HeroSection = () => {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ duration: 1, delay: 2 }}
-          className="absolute bottom-10 left-1/2 transform -translate-x-1/2"
+          className="absolute -bottom-20 left-1/2 transform -translate-x-1/2 "
         >
           <motion.div
             animate={{ y: [0, 10, 0] }}
